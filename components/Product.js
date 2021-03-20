8e2ac78c20804282d15599b626f29ba37239b965
Jahidul Islam Fakir <jifakir@outlook.com>
2021-03-20 11:23:09 +0800

    Added api

--- components/Product.js
+++ components/Product.js
@@ -1,9 +1,10 @@
 import { useRouter } from 'next/router';
 import Image from 'next/image';
 
-const Product = ({id}) => {
+const Product = ({item}) => {
 
     const location = useRouter();
+    const { id, title, image, price, discount} = item;
 
     return (
         <div onClick={() => location.push(`/products/${id}`)} className="group w-full rounded-md bg-white cursor-pointer">
@@ -11,16 +12,16 @@ const Product = ({id}) => {
                 <div className="w-full overflow-hidden">
                     <img
                     className="w-full z-0 h-full transition-transform duration-500 transform group-hover:scale-110" 
-                    src="https://df17fp68uwcso.cloudfront.net/eyJidWNrZXQiOiJtZWRpYS5ldmFseS5jb20uYmQiLCJrZXkiOiJtZWRpYS9pbWFnZXMvNzljMDAyODEwMjEzLXJ0Yi0wMDgucG5nIiwiZWRpdHMiOnsicmVzaXplIjp7IndpZHRoIjo0MDAsImhlaWdodCI6NDAwLCJmaXQiOiJjb250YWluIn0sImJhY2tncm91bmQiOnsiciI6MjU1LCJnIjoyNTUsImIiOjI1NSwiYWxwaGEiOjF9LCJmbGF0dGVuIjp0cnVlLCJqcGVnIjp7InF1YWxpdHkiOjkwfX19" 
+                    src={image} 
                     alt="Product Snapshot" 
                     />
                 </div>
                 <div className="description space-y-2 w-full">
-                    <p className="title text-gray-500">
-                        Xiaomi Polyester Backpack - Magenta - (RTB-008)
+                    <p className="title text-gray-500 text-xs md:text-base">
+                        Xiaomi Polyester Backpack - Magenta - (RTB-008) {title}
                     </p>
-                    <h3 className="price line-through text-gray-300">&#2547;1070</h3>
-                    <h3 className="discounted-price text-gray-500 font-bold">&#2547;1070</h3>
+                    <h3 className="price line-through text-gray-300 text-sm md:text-base">&#2547;{price}</h3>
+                    <h3 className="discounted-price text-gray-500 text-sm md:text-base font-bold">&#2547;{Math.round(price - price * (discount / 100))}</h3>
                 </div>
             </div>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,30 @@
-import Product from '../components/Product'
-import SectionHeader from '../components/ScectionHeader'
-import { shop } from '../products';
-export default function Home() {
+import Product from '../components/Product';
+import SectionHeader from '../components/ScectionHeader';
+
+
+export default function Home({products}) {
+  
   return (
     <div className='w-full container px-2 mx-auto'>
       <h1 className='w-full text-center'>Product List</h1>
       <SectionHeader title="All Products">
         <div className="w-full grid grid-cols-2 py-5 pb-10 md:pb-5 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-3">
           {
-            shop.products.map((item, idx) => <Product key={idx} id={idx} />)
+            products.map((item, idx) => <Product key={idx} item={item} />)
           }
         </div>
       </SectionHeader>
     </div>
   )
-}
+};
+
+export async function getStaticProps () {
+  const res = await fetch('http://localhost:3000/api/products');
+  const data = await res.json();
+  console.log(process.env);
+  return {
+    props: {
+      products: data
+    }
+  }
+};
